e2e68e7640f5bbb0891eb29a146fb02b4a8710cd
Zain <zain@adtracktiv.io>
2024-08-11 03:12:12 +0500

    feat: added dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@/components/ui/button';
+import { ThemeToggle } from '@/components/app/theme-toggle';
 
 export default function Home() {
   return (
     <main className="p-4">
       <p className="text-xl font-bold">Zain's Portfolio Projects</p>
-      <Button>Click Here</Button>
+
+      <ThemeToggle />
     </main>
   );
 }
